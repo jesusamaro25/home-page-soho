955ff0d7d7aad4c64aa60a0794a9c7a47cc5a3e0
Jesus Amaro <jesusamaro1995@gmail.com>
2022-04-10 23:55:04 -0400

    ready to production

--- src/app/home/components/featured-projects/featured-projects.component.ts
+++ src/app/home/components/featured-projects/featured-projects.component.ts
@@ -9,64 +9,23 @@ import { Project } from 'src/app/models/project.model';
 })
 export class FeaturedProjectsComponent implements OnInit {
 
- // public projects: Array<Project> = [];
+ public projects: Array<Project> = [];
 
   constructor(private _apiService: ApiService) { }
 
-  public projects = [
-    {
-        "id": 1,
-        "image": "http://imgfz.com/i/yE7cwre.png",
-        "logo": "http://imgfz.com/i/4dkFqCH.png",
-        "title": "Sitio publico y privado",
-        "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis",
-        "tags": [
-            "usabilidad",
-            "ui",
-            "ux",
-            "test con usuarios"
-        ]
-    },
-    {
-        "id": 2,
-        "image": "http://imgfz.com/i/iyeXRuB.png",
-        "logo": "http://imgfz.com/i/FHfitQN.png",
-        "title": "Sitios web 2017",
-        "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus",
-        "tags": [
-            "responsive",
-            "ui",
-            "ux"
-        ]
-    },
-    {
-        "id": 3,
-        "image": "http://imgfz.com/i/Gk3oBgy.png",
-        "logo": "http://imgfz.com/i/xodDGEs.png",
-        "title": "TV App",
-        "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis",
-        "tags": [
-            "usabilidad",
-            "ui",
-            "ux",
-            "test con usuarios"
-        ]
-    }
-]
-
   ngOnInit(): void {
- //   this.getAllProjects();
+    this.getAllProjects();
   }
 
-  // getAllProjects(): void {
-  //   this._apiService.getAll(`featured/projects`).subscribe((response) => {
+  getAllProjects(): void {
+    this._apiService.getAll(`featured/projects`).subscribe((response) => {
 
-  //     const { projects } = response;
+      const { projects } = response;
 
-  //     this.projects = projects;
-  //   }, error => {
-  //     console.error('Error', error);
-  //   })
-  // }
+      this.projects = projects;
+    }, error => {
+      console.error('Error', error);
+    })
+  }
 
 }
